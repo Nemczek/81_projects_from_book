1e72ed1511e9908d398716e9e0ff2cbed5fb7a1e
Nemczek <p.baran890@gmail.com>
2022-07-26 16:38:07 +0200

    almost finished my approach and corrected README

---
+++ Project_1/project_1_my_approach.py
@@ -0,0 +1,73 @@
+import random
+
+from Project_1.project_1 import MAX_GUESSES
+
+NUM_OF_DIGITS = 3
+GUESSES = 10
+
+def main():
+    print(f"""Welcome to Bagle game!
+    I have a three digit number in my mind, try to guess it !
+    You have {GUESSES} number of guesses.
+    Good luck!
+    """)
+    secNum = generateNumber()
+
+    num_of_tries = 0
+    while True:
+        if num_of_tries != MAX_GUESSES:
+            guess = getGuess()
+            num_of_tries += 1
+
+
+
+
+
+def getGuess():
+    guess = input("> ")
+    
+    while True:
+        if len(guess) == NUM_OF_DIGITS and guess.isdecimal():
+            break
+        print("You typed wrong asnwer! Please remember that asnwer should be a three digit number.")
+        guess = input("> ")
+    
+    return guess
+
+def generateNumber():
+    """Function to generate number"""
+    number = []
+    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    random.shuffle(numbers)
+
+    for i in numbers:
+        if i not in number and len(number) != NUM_OF_DIGITS:
+            number.append(i)
+    
+    return "".join(number)
+
+def giveHints(generatedNumber, guess):
+    """Function to generate hints based on given guess"""
+    hints = []
+    for i in enumerate(guess):
+        if guess[i] == generatedNumber[i]:
+            hints.append("Fermi")
+        elif guess[i] in generatedNumber:
+            hints.append("Pico")
+        else:
+            return "Bagle"
+    return hints
+
+
+
+
+
+"""
+Plan
+1. Wytłumacz o co chodzi w grze
+2. funkcja do losowania liczby (najlepiej bez powtórzeń)
+3. funkcja do pobierania odpowiedzi
+4. sprawdzać aby dane się zgadzały
+5. funkcja do printowania podpowiedzi
+6. Mainloop ze sprawdzaniem odpowiedzi
+"""
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This repository contains all 81 projects from "Big book of small Python pojects"
 wirtten by Al Sweigart. All projects will contain 3 files:
 1. First file will contain code straight from book
-2. Second file is going to be README
+2. Second file is going to be README with instructions
 3. Last one is going to be my attempt to write project all by myself
 
 I will stick to the instructions provided in book. Firstly I will rewrite project from book and than try to do it myself. I'm doing it mainly to train my Git skills and practice some Python.
